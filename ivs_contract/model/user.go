7ea96d1eb2659db446363db3afc7cf86cd0b1658
kine23 <109943751+kine23@users.noreply.github.com>
2023-05-10 10:56:02 +0800

    Add files via upload

--- ivs_contract/model/user.go
+++ ivs_contract/model/user.go
@@ -1,10 +1,18 @@
 package model
 
+type Asset interface {
+	GetKey() string
+}
+
 // User用戶列表
 type User struct {
 	Table    string `json:"table" form:"table"`       //數據標記
 	Username string `json:"username" form:"username"` //用戶帳號
 	Name     string `json:"name" form:"name"`         //姓名
+}
+
+func (u *User) GetKey() string {
+	return u.Username
 }
 
 func (o *User) Index() string {
