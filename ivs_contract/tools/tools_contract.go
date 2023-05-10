fe6528c6d30dde7884ffbdce4fae300f0afba429
kine23 <109943751+kine23@users.noreply.github.com>
2023-05-10 12:55:58 +0800

    Add files via upload

--- ivs_contract/tools/tools_contract.go
+++ ivs_contract/tools/tools_contract.go
@@ -1,69 +1,120 @@
 package tools
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
-
-	"github.com/hyperledger/fabric-chaincode-go/shim"
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
-	"github.com/kine23/nchu_ivslab/ivs_contract/model"
+	"github.com/kine23/nchu_ivslab/model"
 )
 
-// 根據查詢結果生成切片
-func ConstructResultByIterator[T interface{}](resultsIterator shim.StateQueryIteratorInterface) ([]*T, error) {
-	var txs []*T
-	for resultsIterator.HasNext() {
-		queryResult, err := resultsIterator.Next()
-		if err != nil {
-			return nil, err
-		}
-		var tx T
-		err = json.Unmarshal(queryResult.Value, &tx)
-		if err != nil {
-			return nil, err
-		}
-		txs = append(txs, &tx)
+// buildQueryString 用於構建用於查詢的JSON字符串
+func buildQueryString(table, key, value string) string {
+	var buffer bytes.Buffer
+	buffer.WriteString(`{"selector":{"table":"`)
+	buffer.WriteString(table)
+	buffer.WriteString(`"`)
+
+	if key != "" && value != "" {
+		buffer.WriteString(`,"`)
+		buffer.WriteString(key)
+		buffer.WriteString(`":"`)
+		buffer.WriteString(value)
+		buffer.WriteString(`"`)
 	}
-	fmt.Println("select result length: ", len(txs))
-	return txs, nil
+
+	buffer.WriteString("}}")
+	return buffer.String()
 }
 
-// 根據查詢字串查詢
-func SelectByQueryString[T interface{}](ctx contractapi.TransactionContextInterface, queryString string) ([]*T, error) {
+// SelectByQueryString 通用查詢功能
+func SelectByQueryString[T model.Asset](ctx contractapi.TransactionContextInterface, queryString string) ([]*T, error) {
 	resultsIterator, err := ctx.GetStub().GetQueryResult(queryString)
 	if err != nil {
 		return nil, err
 	}
 	defer resultsIterator.Close()
 
-	return ConstructResultByIterator[T](resultsIterator)
+	var results []*T
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return nil, err
+		}
+		var item T
+		err = json.Unmarshal(queryResponse.Value, &item)
+		if err != nil {
+			return nil, err
+		}
+		results = append(results, &item)
+	}
+	return results, nil
 }
 
-// 根據查詢字串分頁查詢
-func SelectByQueryStringWithPagination[T interface{}](ctx contractapi.TransactionContextInterface, queryString string, pageSize int32, bookmark string) (*model.PaginatedQueryResult[T], error) {
-	resultsIterator, responseMetadata, err := ctx.GetStub().GetQueryResultWithPagination(queryString, pageSize, bookmark)
+// SelectByQueryStringWithPagination 通用分頁查詢功能
+func SelectByQueryStringWithPagination[T model.Asset](ctx contractapi.TransactionContextInterface, queryString string, pageSize int32, bookmark string) (*model.PaginatedQueryResult[T], error) {
+	resultsIterator, metadata, err := ctx.GetStub().GetQueryResultWithPagination(queryString, pageSize, bookmark)
 	if err != nil {
 		return nil, err
 	}
 	defer resultsIterator.Close()
-	var txs []T
+
+	var results []T
 	for resultsIterator.HasNext() {
-		queryResult, err := resultsIterator.Next()
+		queryResponse, err := resultsIterator.Next()
 		if err != nil {
 			return nil, err
 		}
-		var tx T
-		err = json.Unmarshal(queryResult.Value, &tx)
+		var item T
+		err = json.Unmarshal(queryResponse.Value, &item)
 		if err != nil {
 			return nil, err
 		}
-		txs = append(txs, tx)
+		results = append(results, item)
 	}
-	return &model.PaginatedQueryResult[T]{
-		Records:             txs,
-		FetchedRecordsCount: responseMetadata.FetchedRecordsCount,
-		Bookmark:            responseMetadata.Bookmark,
-	}, nil
+
+	paginatedQueryResult := model.PaginatedQueryResult[T]{
+		Records:             results,
+		FetchedRecordsCount: metadata.FetchedRecordsCount,
+		Bookmark:            metadata.Bookmark,
+	}
+	return &paginatedQueryResult, nil
+}
+
+// SelectHistoryByIndex 通用歷史查詢功能
+func SelectHistoryByIndex[T model.Asset](ctx contractapi.TransactionContextInterface, index string) ([]*model.HistoryQueryResult[T], error) {
+	resultsIterator, err := ctx.GetStub().GetHistoryForKey(index)
+	if err != nil {
+		return nil, err
+	}
+	defer resultsIterator.Close()
+
+	var results []*model.HistoryQueryResult[T]
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return nil, err
+		}
+		var item T
+		err = json.Unmarshal(queryResponse.Value, &item)
+		if err != nil {
+			return nil, err
+		}
+		historyItem := model.HistoryQueryResult[T]{
+			Record:    item,
+			TxId:      queryResponse.TxId,
+			Timestamp: queryResponse.Timestamp,
+			IsDelete:  queryResponse.IsDelete,
+		}
+		results = append(results, &historyItem)
+	}
+	return results, nil
+}
+
+// SelectByIndexAndPagination 通用按索引分頁查詢功能
+func SelectByIndexAndPagination[T model.Asset](ctx contractapi.TransactionContextInterface, table, key, value string, pageSize int32, bookmark string) (*model.PaginatedQueryResult[T], error) {
+	queryString := buildQueryString(table, key, value)
+	return SelectByQueryStringWithPagination[T](ctx, queryString, pageSize, bookmark)
 }
 
 // 交易創建後的所有變化
@@ -88,12 +139,15 @@ func SelectHistoryByIndex[T interface{}](ctx contractapi.TransactionContextInter
 				return nil, err
 			}
 		}
+		timestamp := time.Unix(response.Timestamp.Seconds, int64(response.Timestamp.Nanos)) // 將時間戳轉換為 time.Time 類型
 		record := model.HistoryQueryResult[T]{
 			TxId:      response.TxId,
 			Record:    tx,
 			IsDelete:  response.IsDelete,
+			Timestamp: timestamp,
 		}
 		records = append(records, record)
 	}
 	return records, nil
 }
+
