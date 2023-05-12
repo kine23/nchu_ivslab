12566542cce79a0cff38fc4deb48a2e1561f046d
kine23 <109943751+kine23@users.noreply.github.com>
2023-05-12 16:58:20 +0800

    Update app.go

--- contract-gateway/app.go
+++ contract-gateway/app.go
@@ -10,7 +10,7 @@ import (
 
 const (
 	channelName   = "ivschannel"		// 連接的channel
-	chaincodeName = "ivs_basic"		// 連接的chaincode
+	chaincodeName = "ivs_basic"			// 連接的chaincode
 )
 
 func main() {
@@ -55,24 +55,14 @@ func getAllAssets(contract *client.Contract) {
 }
 
 func getAllUsers(contract *client.Contract) {
-	fmt.Println("Evaluate Transaction: GetAllUsers, function returns all the current users on the ledger")
+fmt.Println("Evaluate Transaction: GetAllUsers, function returns all the current users on the ledger")
 	evaluateResult, err := contract.EvaluateTransaction("GetAllUsers")
-	if err != nil {
-		panic(fmt.Errorf("failed to evaluate transaction: %w", err))
+if err != nil {
+	panic(fmt.Errorf("failed to evaluate transaction: %w", err))
 	}
+	result := formatJSON(evaluateResult)
 
-	// 解析評估結果為使用者清單
-	var users []*User
-	if err := json.Unmarshal(evaluateResult, &users); err != nil {
-		panic(fmt.Errorf("failed to unmarshal users: %w", err))
-	}
-
-	// 輸出所有使用者清單
-	result, err := json.Marshal(users)
-	if err != nil {
-		panic(fmt.Errorf("failed to marshal users: %w", err))
-	}
-	fmt.Printf("*** Result:%s\n", string(result))
+	fmt.Printf("*** Result:%s\n", result)
 }
 
 func formatJSON(data []byte) string {
