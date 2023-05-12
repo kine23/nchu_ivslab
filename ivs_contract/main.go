7f0213f70e0217fc400541498a777975e1d68594
kine23 <109943751+kine23@users.noreply.github.com>
2023-05-12 14:22:18 +0800

    Update main.go

--- ivs_contract/main.go
+++ ivs_contract/main.go
@@ -6,12 +6,31 @@ import (
 )
 
 func main() {
-	chaincode, err := contractapi.NewChaincode(&contract.UserContract{}, &contract.IVSContract{})
+	IVSContract := new(IVSContract)
+	IVSContract.Contract = new(contractapi.Contract)
+
+	userContract := new(UserContract)
+	userContract.Contract = new(contractapi.Contract)
+
+	chaincode, err := contractapi.NewChaincode(IVSContract, userContract)
 	if err != nil {
-		panic(err)
+		fmt.Printf("Error creating chaincode: %s", err.Error())
+		return
 	}
 
 	if err := chaincode.Start(); err != nil {
-		panic(err)
+		fmt.Printf("Error starting chaincode: %s", err.Error())
 	}
+
 }
+
+//func main() {
+//	chaincode, err := contractapi.NewChaincode(&contract.UserContract{}, &contract.IVSContract{})
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	if err := chaincode.Start(); err != nil {
+//		panic(err)
+//	}
+//}
