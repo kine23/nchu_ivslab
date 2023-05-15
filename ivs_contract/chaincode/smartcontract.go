e1c69d2b73a2a3c79de4dc9cd01574b3ef932238
kine23 <109943751+kine23@users.noreply.github.com>
2023-05-15 15:30:14 +0800

    Update smartcontract.go

--- ivs_contract/chaincode/smartcontract.go
+++ ivs_contract/chaincode/smartcontract.go
@@ -17,24 +17,22 @@ type SmartContract struct {
 // golang keeps the order when marshal to json but doesn't order automatically
 // Project項目列表
 type Asset struct {
-	Table               string `json:"table" form:"table"`  //數據庫標記
+	ID                  string `json:"ID"`                  //項目唯一ID
 	Manufacturer        string `json:"Manufacturer"`        //製造商
 	ManufactureLocation string `json:"ManufactureLocation"` //製造地點
 	PartName            string `json:"PartName"`            //零件名稱
 	PartNumber          string `json:"PartNumber"`          //零件批號
 	SerialNumber        string `json:"SerialNumber"`        //產品序號
+	Organization        string `json:"Organization"`        //組織	
 	ManufactureDate     string `json:"ManufactureDate"`     //製造日期
-	Item                string `json:"Item"`                //項目
-	ID                  string `json:"ID"`                  //項目唯一ID
-	Category            string `json:"Category"`            //所屬類別
-	Describes           string `json:"Describes"`           //描述
-	Developer           string `json:"Developer"`           //開發者
-	Organization        string `json:"Organization"`        //組織
+//	Category            string `json:"Category"`            //所屬類別
+//	Describes           string `json:"Describes"`           //描述
+//	Developer           string `json:"Developer"`           //開發者
+
 }
 
 // User用戶列表
 type User struct {
-	Table    string `json:"table" form:"table"`       //數據標記
 	Username string `json:"username" form:"username"` //用戶帳號
 	Name     string `json:"name" form:"name"`         //姓名
 }
