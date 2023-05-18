88146056e0114030d56f26d65f7fa4a790503ebb
kine23 <109943751+kine23@users.noreply.github.com>
2023-05-18 21:33:30 +0800

    Update chaincode_app.go

--- contract-gateway/chaincode_app.go
+++ contract-gateway/chaincode_app.go
@@ -1,3 +1,9 @@
+/*
+Copyright 2021 IBM All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
 package main
 
 import (
@@ -72,81 +78,15 @@ func main() {
 
 	initLedger(contract)
 	getAllAssets(contract)
-	getAllParts(contract)
 	createAsset(contract)
 	readAssetByID(contract)
-	readPartByID(contract)
+	transferAssetAsync(contract)
+//	getAllUsers(contract)
 	getAssetsByRange(contract)
-	getPartsByRange(contract)
-	queryPartsByOwner(contract)
-	queryAssets(contract)
+	queryAssetsByOrganization(contract)
 	queryAssetsWithPagination(contract)
-	getAssetHistory(contract)
+	getAssetHistory(contract)	
 	exampleErrorHandling(contract)
-	
-	createPart(contract, PartArgs{
-    		PID: "IVSLAB-S23FA0002",
-    		Manufacturer: "Security.Co",
-    		ManufactureLocation: "Taiwan",
-    		PartName: "SecurityChip-v1",
-    		PartNumber: "SPN3R1C00AA2",
-    		Organization: "Security-Org",
-    		ManufactureDate: "2023-05-17",
-		})
-
-	createPart(contract, PartArgs{
-    		PID: "IVSLAB-N23FA0002",
-    		Manufacturer: "Network.Co",
-    		ManufactureLocation: "Taiwan",
-    		PartName: "NetworkChip-v1",
-    		PartNumber: "NPN3R1C00AA2",
-    		Organization: "Network-Org",
-    		ManufactureDate: "2023-05-17",
-		})
-
-	createPart(contract, PartArgs{
-    		PID: "IVSLAB-C23FA0002",
-    		Manufacturer: "CMOS.Co",
-    		ManufactureLocation: "USA",
-    		PartName: "CMOSChip-v1",
-    		PartNumber: "CPN3R1C00AA2",
-    		Organization: "CMOS-Org",
-    		ManufactureDate: "2023-05-17",
-		})
-
-	createPart(contract, PartArgs{
-    		PID: "IVSLAB-V23FA0002",
-    		Manufacturer: "VideoCodec.Co",
-   		ManufactureLocation: "USA",
-    		PartName: "VideoCodecChip-v1",
-    		PartNumber: "VPN3R1C00AA2",
-    		Organization: "VideoCodec-Org",
-    		ManufactureDate: "2023-05-17",
-		})
-	
-	transferPartAsync(contract, TransferPartArgs{
-    		PID: "IVSLAB-S23FA0002",
-    		Organization: "Brand-Org",
-    		ManufactureDate: "2023-05-17",
-		})
-
-	transferPartAsync(contract, TransferPartArgs{
-    		PID: "IVSLAB-N23FA0002",
-    		Organization: "Brand-Org",
-    		ManufactureDate: "2023-05-17",
-		})
-
-	transferPartAsync(contract, TransferPartArgs{
-    		PID: "IVSLAB-C23FA0002",
-    		Organization: "Brand-Org",
-    		ManufactureDate: "2023-05-17",
-		})
-
-	transferPartAsync(contract, TransferPartArgs{
-    		PID: "IVSLAB-V23FA0002",
-    		Organization: "Brand-Org",
-    		ManufactureDate: "2023-05-17",
-		})	
 }
 
 // newGrpcConnection creates a gRPC connection to the Gateway server.
@@ -229,18 +169,6 @@ func initLedger(contract *client.Contract) {
 	fmt.Printf("*** Transaction committed successfully\n")
 }
 
-func getAllParts(contract *client.Contract) {
-	fmt.Println("\n--> Evaluate Transaction: GetAllParts, function returns all the current assets on the ledger")
-
-	evaluateResult, err := contract.EvaluateTransaction("GetAllParts")
-	if err != nil {
-		panic(fmt.Errorf("failed to evaluate transaction: %w", err))
-	}
-	result := formatJSON(evaluateResult)
-
-	fmt.Printf("*** Result:%s\n", result)
-}
-
 // Evaluate a transaction to query ledger state.
 func getAllAssets(contract *client.Contract) {
 	fmt.Println("\n--> Evaluate Transaction: GetAllAssets, function returns all the current assets on the ledger")
@@ -253,34 +181,50 @@ func getAllAssets(contract *client.Contract) {
 
 	fmt.Printf("*** Result:%s\n", result)
 }
-type PartArgs struct {
-    PID                  string
-    Manufacturer         string
-    ManufactureLocation  string
-    PartName             string
-    PartNumber           string
-    Organization         string
-    ManufactureDate      string
+
+// Evaluate a transaction to query ledger state.
+//func getAllUsers(contract *client.Contract) {
+//	fmt.Println("\n--> Evaluate Transaction: GetAllUsers, function returns all the current users on the ledger")
+//
+//	evaluateResult, err := contract.EvaluateTransaction("GetAllUsers")
+//	if err != nil {
+//		panic(fmt.Errorf("failed to evaluate transaction: %w", err))
+//	}
+//	result := formatJSON(evaluateResult)
+//
+//	fmt.Printf("*** Result:%s\n", result)
+//}
+
+// Submit a transaction synchronously, blocking until it has been committed to the ledger.
+func createAsset(contract *client.Contract) {
+	fmt.Printf("\n--> Submit Transaction: CreateAsset, creates new asset with ID, Manufacturer, ManufactureLocation, PartName, PartNumber, SerialNumber, Organization, ManufactureDate\n")
+
+	_, err := contract.SubmitTransaction("CreateAsset", "IVSLAB-N23FA02", "Network.co", "Taiwan", "NetworkChip-v1", "NPN301AA", "NSN30A11AA", "Network-Org", "2023-05-15")
+	if err != nil {
+		panic(fmt.Errorf("failed to submit transaction: %w", err))
+	}
+
+	fmt.Printf("*** Transaction committed successfully\n")
 }
 
-func createPart(contract *client.Contract, args PartArgs) {
-    fmt.Printf("\n--> Submit Transaction: CreatePart, creates new part with PID, Manufacturer, ManufactureLocation, PartName, PartNumber, ManufactureDate, Organization \n")
-    _, err := contract.SubmitTransaction("CreatePart", args.PID, args.Manufacturer, args.ManufactureLocation, args.PartName, args.PartNumber, args.Organization, args.ManufactureDate)
-    if err != nil {
-        panic(fmt.Errorf("failed to submit transaction: %w", err))
-    }
-    fmt.Printf("*** Transaction committed successfully\n")
+// Evaluate a transaction by assetID to query ledger state.
+func readAssetByID(contract *client.Contract) {
+	fmt.Printf("\n--> Evaluate Transaction: ReadAsset, function returns asset attributes\n")
+
+	evaluateResult, err := contract.EvaluateTransaction("ReadAsset", "IVSLAB-N23FA02")
+	if err != nil {
+		panic(fmt.Errorf("failed to evaluate transaction: %w", err))
+	}
+	result := formatJSON(evaluateResult)
+
+	fmt.Printf("*** Result:%s\n", result)
 }
 
-type TransferPartArgs struct {
-    PID                  string
-    Organization         string
-    ManufactureDate      string
-}
-
-func transferPartAsync(contract *client.Contract, args TransferPartArgs) {
-	fmt.Printf("\n--> Async Submit Transaction: TransferPart, updates existing part Organization and TransferDate")
-	submitResult, commit, err := contract.SubmitAsync("TransferPart", client.WithArguments(args.PID, args.ManufactureDate, args.Organization))
+// Submit transaction asynchronously, blocking until the transaction has been sent to the orderer, and allowing
+// this thread to process the chaincode response (e.g. update a UI) without waiting for the commit notification
+func transferAssetAsync(contract *client.Contract) {
+	fmt.Printf("\n--> Async Submit Transaction: TransferAsset, updates existing asset Organization and TransferDate")
+	submitResult, commit, err := contract.SubmitAsync("TransferAsset", client.WithArguments("IVSLAB-N23FA02", "2023-05-16", "Brand-Org"))
 	if err != nil {
 		panic(fmt.Errorf("failed to submit transaction asynchronously: %w", err))
 	}
@@ -295,74 +239,32 @@ func transferPartAsync(contract *client.Contract, args TransferPartArgs) {
 	fmt.Printf("*** Transaction committed successfully\n")
 }
 
-// Submit a transaction synchronously, blocking until it has been committed to the ledger.
-func createAsset(contract *client.Contract) {
-	fmt.Printf("\n--> Submit Transaction: CreateAsset, creates new asset with ID, MadeBy, MadeIn, SerialNumber, SecurityChip, NetworkChip, CMOSChip, VideoCodecChip, ProductionDate \n")
-
-	_, err := contract.SubmitTransaction("CreateAsset", "IVSLAB-PVC23FG0002", "Bard.Co", "Taiwan", "IVSPN902300AACDC02", "IVSLAB-S23FA0002", "IVSLAB-N23FA0002", "IVSLAB-C23FA0002", "IVSLAB-V23FA0002", "2023-05-18")
-	if err != nil {
-		panic(fmt.Errorf("failed to submit transaction: %w", err))
-	}
-
-	fmt.Printf("*** Transaction committed successfully\n")
-}
-
-// Evaluate a transaction by assetID to query ledger state.
-func readPartByID(contract *client.Contract) {
-	fmt.Printf("\n--> Evaluate Transaction: ReadPart, function returns asset attributes\n")
-
-	evaluateResult, err := contract.EvaluateTransaction("ReadPart", "IVSLAB-S23FA0002")
-	if err != nil {
-		panic(fmt.Errorf("failed to evaluate transaction: %w", err))
-	}
-	result := formatJSON(evaluateResult)
-
-	fmt.Printf("*** Result:%s\n", result)
-}
-
-func readAssetByID(contract *client.Contract) {
-	fmt.Printf("\n--> Evaluate Transaction: ReadAsser, function returns asset attributes\n")
-
-	evaluateResult, err := contract.EvaluateTransaction("ReadAsset", "IVSLAB-PVC23FG0001")
-	if err != nil {
-		panic(fmt.Errorf("failed to evaluate transaction: %w", err))
-	}
-	result := formatJSON(evaluateResult)
-
-	fmt.Printf("*** Result:%s\n", result)
-}
-
-func getPartsByRange(contract *client.Contract) {
-	fmt.Println("\n--> Evaluate Transaction: GetPartsByRange, function returns all the current assets on the ledger")
-
-	evaluateResult, err := contract.EvaluateTransaction("GetPartsByRange", "IVSLAB-V23FA0001", "IVSLAB-V23FA0004")
-	if err != nil {
-		panic(fmt.Errorf("failed to evaluate transaction: %w", err))
-	}
-	result := formatJSON(evaluateResult)
-
-	fmt.Printf("*** Result:%s\n", result)
-}
-
 func getAssetsByRange(contract *client.Contract) {
 	fmt.Println("\n--> Evaluate Transaction: GetAssetsByRange, function returns all the current assets on the ledger")
 
-	evaluateResult, err := contract.EvaluateTransaction("GetAssetsByRange", "IVSLAB-PVC23FG0001", "IVSLAB-PVC23FG0003")
+	evaluateResult, err := contract.EvaluateTransaction("GetAssetsByRange", "IVSLAB-N23FA01", "IVSLAB-N23FA03")
 	if err != nil {
 		panic(fmt.Errorf("failed to evaluate transaction: %w", err))
 	}
+	// 檢查回傳的數據是否為空，只有當數據不為空時才進行格式化
+	if len(evaluateResult) > 0 {
 	result := formatJSON(evaluateResult)
-
+	
 	fmt.Printf("*** Result:%s\n", result)
+	
+	} else {
+         
+         fmt.Println("No assets found in the given range.")
+    }
 }
+func queryAssetsByOrganization(contract *client.Contract) {
+	fmt.Println("\n--> Evaluate Transaction: QueryAssetsByOrganization, function returns all the current assets on the ledger")
 
-func queryPartsByOwner(contract *client.Contract) {
-	fmt.Println("\n--> Evaluate Transaction: QueryPartsByOwner, function returns all the current assets on the ledger")
-
-	evaluateResult, err := contract.EvaluateTransaction("QueryPartsByOwner", "Network-Org")
+	evaluateResult, err := contract.EvaluateTransaction("QueryAssetsByOrganization", "Network-Org")
 	if err != nil {
 		panic(fmt.Errorf("failed to evaluate transaction: %w", err))
 	}
+	
 	// Add a check here for empty result
 	if len(evaluateResult) == 0 {
 		fmt.Println("*** No assets found for the specified organization")
@@ -373,36 +275,17 @@ func queryPartsByOwner(contract *client.Contract) {
 
 	fmt.Printf("*** Result:%s\n", result)
 }
-
-func queryAssets(contract *client.Contract) {
-	fmt.Println("\n--> Evaluate Transaction: QueryAssets, function returns all the current assets on the ledger")
-
-	evaluateResult, err := contract.EvaluateTransaction("QueryAssets", "IVSLAB-PVC23FG0001")
-	if err != nil {
-		panic(fmt.Errorf("failed to evaluate transaction: %w", err))
-	}
-	// Add a check here for empty result
-	if len(evaluateResult) == 0 {
-		fmt.Println("*** No assets found for the specified organization")
-		return
-	}
-
-	result := formatJSON(evaluateResult)
-
-	fmt.Printf("*** Result:%s\n", result)
-}
-
 func queryAssetsWithPagination(contract *client.Contract) {
 	fmt.Println("\n--> Evaluate Transaction: QueryAssetsWithPagination, function returns all the current assets on the ledger")
 
-	evaluateResult, err := contract.EvaluateTransaction("QueryAssetsWithPagination", `{"selector":{"docType":"asset","madeby":"Brand.Co"}, "use_index":["_design/indexMadeByDoc", "indexMadeBy"]}`, "1", "")
+	evaluateResult, err := contract.EvaluateTransaction("QueryAssetsWithPagination", `{"selector":{"docType":"asset","organization":"Brand"}, "use_index":["_design/indexOrganizationDoc", "indexOrganization"]}`, "1", "")
 	if err != nil {
 		panic(fmt.Errorf("failed to evaluate transaction: %w", err))
 	}
 
 	// Check if result is null
 	if evaluateResult == nil {
-		fmt.Println("*** No assets found for the specified madeby")
+		fmt.Println("*** No assets found for the specified organization")
 		return
 	}
 
@@ -413,7 +296,7 @@ func queryAssetsWithPagination(contract *client.Contract) {
 func getAssetHistory(contract *client.Contract) {
 	fmt.Println("\n--> Evaluate Transaction: GetAssetHistory, function returns all the current assets on the ledger")
 
-	evaluateResult, err := contract.EvaluateTransaction("GetAssetHistory", "IVSLAB-PVC23FG0001")
+	evaluateResult, err := contract.EvaluateTransaction("GetAssetHistory", "IVSLAB-N23FA02")
 	if err != nil {
 		panic(fmt.Errorf("failed to evaluate transaction: %w", err))
 	}
@@ -423,9 +306,9 @@ func getAssetHistory(contract *client.Contract) {
 }
 // Submit transaction, passing in the wrong number of arguments ,expected to throw an error containing details of any error responses from the smart contract.
 func exampleErrorHandling(contract *client.Contract) {
-	fmt.Println("\n--> Submit Transaction: UpdateAsset IVSLAB-N23FA03, IVSLAB-N23FA01 does not exist and should return an error")
+	fmt.Println("\n--> Submit Transaction: UpdateAsset IVSLAB-N23FA04, IVSLAB-N23FA04 does not exist and should return an error")
 
-	_, err := contract.SubmitTransaction("UpdateAsset", "IVSLAB-N23FA03", "Network.co", "Taiwan", "NetworkChip-v1", "NPN303AA", "SNN30A13AA", "Network-Org", "2023-05-15")
+	_, err := contract.SubmitTransaction("UpdateAsset", "IVSLAB-N23FA04", "Network.co", "Taiwan", "NetworkChip-v1", "NPN304AA", "SNN30A14AA", "Network-Org", "2023-05-15")
 	if err == nil {
 		panic("******** FAILED to return an error")
 	}
