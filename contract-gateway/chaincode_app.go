dcc3e9db6c5d3a8d4c3664dc795f917374fa7ab0
kine23 <109943751+kine23@users.noreply.github.com>
2023-05-27 17:26:15 +0800

    Update chaincode_app.go

--- contract-gateway/chaincode_app.go
+++ contract-gateway/chaincode_app.go
@@ -187,7 +187,7 @@ func createPart(contract *client.Contract) {
 		fmt.Printf("failed to submit transaction: %s\n", err)
 		return
 	}
-	fmt.Printf("*** Transaction committed Part %s\n created successfully\n")
+	fmt.Printf("*** Transaction committed Part part%d created successfully\n")
 }
 
 // Submit transaction asynchronously, blocking until the transaction has been sent to the orderer, and allowing
